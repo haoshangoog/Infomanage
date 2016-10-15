1a9a408dbcff79976dc9110e6c17a390e5db2722
Yang <1270560069@qq.com>
2016-10-15 22:38:25 +0800

    【ADD】 完成修改 目录的模态框

--- src/main/webapp/jsp/js/treeCatalogue.js
+++ src/main/webapp/jsp/js/treeCatalogue.js
@@ -1,6 +1,6 @@
 var FRISTCATALOGUEID = null;
 
-var EDITFlAG = $("#editFlag").is(':checked');
+// var EDITFlAG = $("#editFlag").is(':checked');
 
 $.ajax({
     type: "post",
@@ -40,8 +40,11 @@ $.ajax({
 
 function showChild(obj){
     parentsId = $(obj).attr("value");
+    parentsName = $(obj).text();
+
     console.log(" obj --------")
     console.log(obj);
+
     var thisNode = $(obj);
     var head = "<ul  style='display: none'>";
     var end  = "</ul>";
@@ -65,6 +68,15 @@ function showChild(obj){
             }
             console.log($(thisNode))
             $(thisNode).parent('li').append(head+middel+end);
+
+            // 判断显示的页面
+            if($("#editFlag").is(':checked')){
+                console.log("应该显示 【编辑目录】  页面");
+                ShowEditCatalogue(parentsName, msg);
+            }else {
+                console.log("应该显示 【编辑内容】  页面");
+            }
+
         }
     });
 
@@ -93,3 +105,86 @@ function showChild(obj){
         $(obj).siblings('ul').children('li').children('a').removeClass('inactives');
     }
 };
+
+
+// 显示 编辑目录页面
+function ShowEditCatalogue(parentsName , childrenCatalogue) {
+
+    console.log("显示 编辑目录页面  ==> ShowEditCatalogue");
+    console.log("parentsName: "+parentsName);
+    console.log("childrenCatalogue： ");
+    console.log(childrenCatalogue);
+    //  给 根目录赋值
+    $("#rootCatagolueName").text("根目录： " + parentsName );
+    //  给 子目录赋值
+    $("#childrenCatalogueList").children().remove();
+    $.each(childrenCatalogue, function(i,catalogue){
+        console.log(catalogue);
+        var child =
+            '<tr>' +
+            '<td>' + catalogue.sequence + '</td>' +
+            '<td>' + catalogue.catalogueName + '</td>' +
+            '<td>' +
+            '<button type="button" class="btn btn-default btn-sm"' +
+                    'onclick="editButton(' + catalogue.id +',\'' + catalogue.catalogueName + "'," + catalogue.sequence + ')">编辑</button>' +
+            '<button type="button" class="btn btn-default btn-sm" onclick="deleteButton(' + catalogue.id + ')">删除</button>' +
+            '</td>' +
+            '</tr>' ;
+        $("#childrenCatalogueList").append(child);
+    });
+
+
+
+}
+
+// 显示 内容页 页面
+function ShowEditContextPage(parentsId) {
+
+    console.log("显示 内容页 页面  ==> ShowEditContextPage");
+
+}
+
+// 显示 编辑目录页面的 模态框
+function editButton(catalogueID, catalogueName, sequence) {
+    console.log("------ editButton 方法");
+    console.log(catalogue)
+    if(catalogue){
+        // 1 .  显示模态框
+        $('#editModal').modal('show');
+        // 2 . 模态框 头的显示
+        $('#editModalLabel').text( "编辑： " + catalogueName ) ;
+        // 3 . 添加默认的 值
+        $('#editCatalogueName').val( catalogueName ) ;
+        $('#editSequence').val( sequence ) ;
+        $('#editCatalogueID').val(catalogueID)
+    }
+}
+// 提交更改信息
+function editCommit() {
+    console.log("------ editCommit 方法");
+    var catalogueID = $('#editCatalogueID').val();
+    var catalogueName = $('#editCatalogueName').val();
+    var sequence = $('#editSequence').val() ;
+
+    $.ajax({
+        type: "post",
+        data: {"catalogueId":catalogueID,"catalogueName":catalogueName,"sequence":sequence},
+        url: "http://localhost:8080/testPlanCatalogue/updateTestPlanCatalogue",
+        async: false,
+        dataType: 'html',
+        error : function() {
+            alert("editCommit 方法 出错");
+        },
+        success: function (msg) {
+            if(msg == "0105"){
+                alert("更新成功");
+            }else {
+                alert("更新失败");
+            }
+        }
+    });
+
+    $('#editModal').modal('hide');
+
+
+}
