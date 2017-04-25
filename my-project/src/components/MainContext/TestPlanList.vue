fed9ff3c9444a52e0fd37b85abffe3524d667aa8
yangyi <yangyi6@xiaomi.com>
2017-04-25 16:48:35 +0800

    [ FIX ]  修改 测试方案列表页面样式

--- my-project/src/components/MainContext/TestPlanList.vue
+++ my-project/src/components/MainContext/TestPlanList.vue
@@ -2,21 +2,31 @@
   <div id="testPlanList">
     <el-row :gutter="10">
       <el-col :xs="8" :sm="6" :md="4" :lg="3">
-        <div class="grid-content bg-purple-light">
-          <el-button type="text" @click="addTestPlan">添加测试方案</el-button>
-        </div>
+        <el-menu mode="vertical" default-active="1" class="">
+          <el-menu-item-group title="基本功能">
+            <el-menu-item index="1" @click="addTestPlan"><i class="el-icon-message"></i>添加测试方案</el-menu-item>
+            <el-menu-item index="2"><i class="el-icon-message"></i>其他</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="扩展功能">
+            <el-menu-item index="3"><i class="el-icon-message"></i>其他</el-menu-item>
+            <el-menu-item index="4"><i class="el-icon-message"></i>其他</el-menu-item>
+          </el-menu-item-group>
+        </el-menu>
       </el-col>
+
       <el-col :xs="8" :sm="12" :md="16" :lg="18">
         <div class="grid-content bg-purple-light">
           <div v-for="o in testPlanList">
-            <el-card class="box-card">
+            <el-card class="">
               <router-link :to="'/testPlanForm/'+ o.id">
                 <div class="text item">
                   {{o.planname}}
                 </div>
               </router-link>
-              <el-button type="text" @click="editTestPlan(o.id)">编辑</el-button>
-              <el-button type="text" @click="deleteTestPlan(o.id)">删除</el-button>
+              <div style="float: right">
+                <el-button type="text" @click="editTestPlan(o.id, o.planname)">修改测试方案名称</el-button>
+                <el-button type="text" @click="deleteTestPlan(o.id)">删除测试方案</el-button>
+              </div>
             </el-card>
           </div>
           <div class="block">
@@ -30,6 +40,7 @@
           </div>
         </div>
       </el-col>
+
       <el-col :xs="8" :sm="6" :md="4" :lg="3">
         <div class="grid-content bg-purple-light"></div>
       </el-col>
@@ -40,7 +51,7 @@
   export default{
     data () {
       return {
-        apiUrl: 'http://localhost:8085/testPlan/pageingTestPlan',
+        apiUrl: '/testPlan/pageingTestPlan',
         testPlanList: {},
         page: {
           'currentPage': 1,
@@ -62,9 +73,10 @@
         }).then(({value}) => {
           this.addTestPlanReq(value)
         }).catch(() => {
-          this.$message({
-            type: 'info',
-            message: '取消输入'
+          this.$notify({
+            title: '成功',
+            message: '删除成功!',
+            type: 'info'
           })
         })
       },
@@ -72,57 +84,65 @@
         var vm = this
         var formData = new window.FormData()
         formData.append('planName', planName)
-        vm.$http.post('http://localhost:8085/testPlan/createTestPlan', formData).then((response) => {
+        vm.$http.post('/testPlan/createTestPlan', formData).then((response) => {
           console.log(response.body)
           if (response.body === '0101') {
-            this.$message({
-              type: 'success',
-              message: '创建成功'
+            this.$notify({
+              title: '成功',
+              message: '创建成功!',
+              type: 'success'
             })
           } else {
-            this.$message({
-              type: 'info',
-              message: '创建失败'
+            this.$notify({
+              title: '失败',
+              message: '创建失败!',
+              type: 'waring'
             })
           }
         })
       },
-      editTestPlan (id) {
-        console.log('编辑测试方案的ID是: ' + id)
-        this.$prompt('请输入测试方案名:', '提示', {
+
+      editTestPlan (id, planName) {
+        console.log('编辑测试方案的ID是: ' + id + ' / ' + planName)
+        this.$prompt('请修改测试方案名:', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
+          inputValue: planName,
 //          inputPattern: /[\w!#$%&'*+/=?^_`{|}~-]+(?:\.[\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\w](?:[\w-]*[\w])?\.)+[\w](?:[\w-]*[\w])?/,
           inputErrorMessage: '测试方案名格式不正确'
         }).then(({value}) => {
           this.editTestPlanReq(id, value)
         }).catch(() => {
-          this.$message({
-            type: 'info',
+          this.$notify.info({
+            title: '消息',
             message: '取消输入'
           })
         })
       },
+
       editTestPlanReq (id, planName) {
         var vm = this
         var formData = new window.FormData()
         formData.append('planNameId', id)
         formData.append('planName', planName)
-        vm.$http.post('http://localhost:8085/testPlan/updateTestPlan', formData).then((response) => {
+        vm.$http.post('/testPlan/updateTestPlan', formData).then((response) => {
           console.log(response.body)
           if (response.body === '0105') {
-            this.$message({
-              type: 'success',
-              message: '更新成功'
+            this.$notify({
+              title: '成功',
+              message: '修改成功!',
+              type: 'success'
             })
           } else {
-            this.$message({
-              type: 'info',
-              message: '更新失败'
+            this.$notify({
+              title: '失败',
+              message: '修改失败!',
+              type: 'warning'
             })
           }
         })
       },
+
       deleteTestPlan (id) {
         this.$confirm('此操作将删除该测试方案, 是否继续?', '提示', {
           confirmButtonText: '确定',
@@ -130,13 +150,9 @@
           type: 'warning'
         }).then(() => {
           this.deleteTestPlanReq(id)
-          this.$message({
-            type: 'success',
-            message: '删除成功!'
-          })
         }).catch(() => {
-          this.$message({
-            type: 'info',
+          this.$notify.info({
+            title: '消息',
             message: '已取消删除'
           })
         })
@@ -145,17 +161,19 @@
         var vm = this
         var formData = new window.FormData()
         formData.append('planNameId', id)
-        vm.$http.post('http://localhost:8085/testPlan/deleteTestPlan', formData).then((response) => {
+        vm.$http.post('/testPlan/deleteTestPlan', formData).then((response) => {
           console.log(response.body)
           if (response.body === '0103') {
-            this.$message({
-              type: 'success',
-              message: '删除成功'
+            this.$notify({
+              title: '成功',
+              message: '删除成功!',
+              type: 'success'
             })
           } else {
-            this.$message({
-              type: 'info',
-              message: '删除失败'
+            this.$notify({
+              title: '失败',
+              message: '删除失败!',
+              type: 'waring'
             })
           }
         })
@@ -189,15 +207,11 @@
 
 <style>
   .text {
-    font-size: 14px;
+    font-size: 20px;
   }
 
   .item {
-    padding: 18px 0;
-  }
-
-  .box-card {
-    width: 480px;
+    padding: 10px 0;
   }
 
   .bg-purple-light {
