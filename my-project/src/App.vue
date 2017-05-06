c5469304c254de119fdd392aa6fa69d507bb2242
yangyi <yangyi6@xiaomi.com>
2017-05-06 17:27:28 +0800

    [ADD] 自动检测登录状态

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -8,6 +8,47 @@
     </router-view>
   </div>
 </template>
+<script>
+  import {mapGetters} from 'vuex'
+  export default{
+    computed: {
+      ...mapGetters(['role'])
+    },
+    mounted () {
+      this.isLoginReq()
+    },
+    methods: {
+      isLoginReq () {
+        var vm = this
+        vm.$http.post('/user/isLogin').then((response) => {
+          if (response.body === '1107') {
+            console.log('用户没有进行登录！')
+          } else {
+            return response.json()
+          }
+        }).then((json) => {
+          if (json === null) {
+            this.$store.commit('setRoleGuest')
+          } else {
+            var identity = json.identity
+            if (identity === '1') {
+              this.$store.commit('setRoleUser')
+            } else if (identity === '2') {
+              this.$store.commit('setRoleAdmin')
+            } else {
+              this.$store.commit('setRoleGuest')
+            }
+            this.$message({
+              type: 'info',
+              message: '登录成功'
+            })
+            this.$router.push('/TestPlanList')
+          }
+        })
+      }
+    }
+  }
+</script>
 <style>
   body {
     margin : 0;
